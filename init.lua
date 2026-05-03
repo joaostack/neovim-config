83bf1fda466589056ab90b73dc8d3dd5275a87c1
joaostack <83355344+joaostack@users.noreply.github.com>
2026-05-03 08:43:48 -0300

    add: desc

--- init.lua
+++ init.lua
@@ -97,6 +97,6 @@ vim.diagnostic.config({
 vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "LSP Diagnostics" })
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
+vim.keymap.set("n", "<leader>c", vim.diagnostic.setloclist, { desc = "Close float window" })
 
 vim.cmd("colorscheme catppuccin")

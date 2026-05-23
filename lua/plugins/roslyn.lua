da0f5739e4c418134523e42b2d43341015de2f2d
João Henryque <83355344+joaostack@users.noreply.github.com>
2026-05-22 22:37:36 -0300

    Refactor Roslyn plugin configuration and dependencies

--- lua/plugins/roslyn.lua
+++ lua/plugins/roslyn.lua
@@ -1,32 +1,36 @@
 return {
-	"seblyng/roslyn.nvim",
-	{
-		"seblyng/roslyn.nvim",
-		---@module 'roslyn.config'
-		---@type RoslynNvimConfig
-		opts = {
-			filewatching = "auto",
-			broad_search = true,
-			silent = true,
-		},
-	},
+        "seblyng/roslyn.nvim",
 
-	config = function()
-		require("roslyn").setup({
-			vim.lsp.config("roslyn", {
-				on_attach = function()
-					print("This will run when the server attaches!")
-				end,
-				settings = {
-					["csharp|inlay_hints"] = {
-						csharp_enable_inlay_hints_for_implicit_object_creation = true,
-						csharp_enable_inlay_hints_for_implicit_variable_types = true,
-					},
-					["csharp|code_lens"] = {
-						dotnet_enable_references_code_lens = true,
-					},
-				},
-			}),
-		})
-	end,
+        dependencies = {
+                "tris203/rzls.nvim",
+        },
+
+        opts = {
+                filewatching = "roslyn",
+                broad_search = true,
+                silent = true,
+        },
+
+        config = function(_, opts)
+                require("roslyn").setup({
+                        filewatching = opts.filewatching,
+                        broad_search = opts.broad_search,
+                        silent = opts.silent,
+
+                        on_attach = function(client, bufnr)
+                                print("Roslyn attached!")
+                        end,
+
+                        settings = {
+                                ["csharp|inlay_hints"] = {
+                                        csharp_enable_inlay_hints_for_implicit_object_creation = true,
+                                        csharp_enable_inlay_hints_for_implicit_variable_types = true,
+                                },
+
+                                ["csharp|code_lens"] = {
+                                        dotnet_enable_references_code_lens = true,
+                                },
+                        },
+                })
+        end,
 }
